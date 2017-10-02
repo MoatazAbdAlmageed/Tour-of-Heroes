623fae511ddc49a579ff41c931884cfbce0a6864
Moataz <moataz.mohammady@gmail.com>
2017-10-02 19:33:00 +0200

    change hero mose move

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -8,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
   title = 'Tour of Heroes';
   constructor() { }
-
   ngOnInit() {
   }
-
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,5 +5,13 @@ export  const HEROES: Hero[] = [
   {id: 2, firstName: 'Ramey  ', lastName: 'Ahmed', email: 'Ramey@gmail.com',imageurl:"/assets/2017-10-02_172353.png"},
   {id: 3, firstName: 'Kareem ', lastName: 'Zaki', email: 'Kareem@mail.com',imageurl:"/assets/2017-10-02_172401.png"},
   {id: 4, firstName: 'Ahmed  ', lastName: 'Zaki', email: 'Ahmed.Zaki@gmail.com',imageurl:"/assets/2017-10-02_172440.png"},
+  {id: 5, firstName: 'Ayman  ', lastName: 'Al', email: 'Ahmed.Zaki@gmail.com',imageurl:"/assets/2017-10-02_172440.png"},
+  {id: 6, firstName: 'user 1  ', lastName: 'Zaki', email: 'Ahmed.Zaki@gmail.com',imageurl:"/assets/2017-10-02_172440.png"},
+  {id: 7, firstName: 'user2  ', lastName: 'Zaki', email: 'Ahmed.Zaki@gmail.com',imageurl:"/assets/2017-10-02_172440.png"},
+  {id: 8, firstName: 'user3  ', lastName: 'Zaki', email: 'Ahmed.Zaki@gmail.com',imageurl:"/assets/2017-10-02_172440.png"},
+  {id: 9, firstName: 'user4  ', lastName: 'Zaki', email: 'Ahmed.Zaki@gmail.com',imageurl:"/assets/2017-10-02_172440.png"},
+  {id: 10, firstName: 'user5  ', lastName: 'Zaki', email: 'Ahmed.Zaki@gmail.com',imageurl:"/assets/2017-10-02_172440.png"},
+  {id: 11, firstName: 'user6  ', lastName: 'Zaki', email: 'Ahmed.Zaki@gmail.com',imageurl:"/assets/2017-10-02_172440.png"},
+  {id: 12, firstName: 'user7  ', lastName: 'Zaki', email: 'Ahmed.Zaki@gmail.com',imageurl:"/assets/2017-10-02_172440.png"},
 ];
 
